3e66037b81a7f498054e6ffd715f2c02f90614a2
Maxim <54853308+SaintShamon@users.noreply.github.com>
2022-05-27 23:17:11 +0300

    fix main page, fix contact page

--- js/template-parts/footer/footer.js
+++ js/template-parts/footer/footer.js
@@ -3,11 +3,16 @@ import $ from 'jquery';
 function footer() {
     if ($(window).width() < 1025) {
         /*Dropdown Menu footer*/
+
         $('footer .footer_menu li.has-sub-menu').click(function () {
             event.preventDefault();
             $(this).attr('tabindex', 1).focus();
             $(this).toggleClass('active');
             $(this).find('.sub-menu').slideToggle(300);
+        });
+
+        $("footer .footer_menu li.has-sub-menu").dblclick(function () {
+            $(this).unbind('click')
         });
 
         $('footer .footer_menu li.has-sub-menu').focusout(function () {
