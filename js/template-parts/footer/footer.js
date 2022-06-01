9d071c8f8199780cfebd59667051f9fcba4127d8
Maxim <54853308+SaintShamon@users.noreply.github.com>
2022-06-02 02:58:04 +0300

    links fix

--- js/template-parts/footer/footer.js
+++ js/template-parts/footer/footer.js
@@ -4,18 +4,18 @@ function footer() {
     if ($(window).width() < 1025) {
         /*Dropdown Menu footer*/
 
-        $('footer .footer_menu li.has-sub-menu').click(function () {
+        $('footer .footer_menu li.menu-item-has-children').click(function () {
             event.preventDefault();
             $(this).attr('tabindex', 1).focus();
             $(this).toggleClass('active');
             $(this).find('.sub-menu').slideToggle(300);
         });
 
-        $("footer .footer_menu li.has-sub-menu").dblclick(function () {
+        $("footer .footer_menu li.menu-item-has-children").dblclick(function () {
             $(this).unbind('click')
         });
 
-        $('footer .footer_menu li.has-sub-menu').focusout(function () {
+        $('footer .footer_menu li.menu-item-has-children').focusout(function () {
             $(this).removeClass('active');
             $(this).find('.sub-menu').slideUp(300);
         });
